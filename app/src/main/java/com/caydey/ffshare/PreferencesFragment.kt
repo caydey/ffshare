c35ca3e805f38fca26663045fc300589fdd8be2e
caydey <caydey@protonmail.com>
2023-01-03 19:08:02 +0000

    Dynamically show "Custom media name" settings option

--- app/src/main/java/com/caydey/ffshare/PreferencesFragment.kt
+++ app/src/main/java/com/caydey/ffshare/PreferencesFragment.kt
@@ -1,25 +1,22 @@
 package com.caydey.ffshare
 
-import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.*
 
-class PreferencesFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+class PreferencesFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-    }
 
-    override fun onResume() {
-        preferenceManager.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
-        super.onResume()
-    }
-
-    override fun onPause() {
-        preferenceManager.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
-        super.onPause()
-    }
-
-    override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
-        //
+        // only show pref_compressed_media_custom_name if pref_compressed_media_name is "Custom"
+        val customMediaNamePreference =
+            findPreference<EditTextPreference>("pref_compressed_media_custom_name")
+        val compressedMediaNamePreference =
+            findPreference<ListPreference>("pref_compressed_media_name")
+        compressedMediaNamePreference?.setOnPreferenceChangeListener { _, value ->
+            customMediaNamePreference?.isVisible = (value == "CUSTOM")
+            true
+        }
+        // trigger update for initial load
+        compressedMediaNamePreference?.callChangeListener(compressedMediaNamePreference.value)
     }
 }
