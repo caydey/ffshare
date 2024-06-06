6475dcc4885e384186f0269898b07d157bb5f906
caydey <caydey@protonmail.com>
2024-06-06 20:15:31 +0100

    improved preferences for custom parameters

--- app/src/main/java/com/caydey/ffshare/PreferencesFragment.kt
+++ app/src/main/java/com/caydey/ffshare/PreferencesFragment.kt
@@ -6,12 +6,20 @@ import androidx.preference.*
 class PreferencesFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-
+        dynamicallyShowCustomName();
+        dynamicallyAddCustomParamTooltips();
+    }
+    private fun dynamicallyAddCustomParamTooltips() {
+        val customParamKeys = arrayOf("pref_custom_video_params", "pref_custom_audio_params", "pref_custom_image_params");
+        for (customParamKey in customParamKeys) {
+            val element = findPreference<EditTextPreference>(customParamKey)
+            element?.summaryProvider = EditTextPreference.SimpleSummaryProvider.getInstance()
+        }
+    }
+    private fun dynamicallyShowCustomName() {
         // only show pref_compressed_media_custom_name if pref_compressed_media_name is "Custom"
-        val customMediaNamePreference =
-            findPreference<EditTextPreference>("pref_compressed_media_custom_name")
-        val compressedMediaNamePreference =
-            findPreference<ListPreference>("pref_compressed_media_name")
+        val customMediaNamePreference = findPreference<EditTextPreference>("pref_compressed_media_custom_name")
+        val compressedMediaNamePreference = findPreference<ListPreference>("pref_compressed_media_name")
         compressedMediaNamePreference?.setOnPreferenceChangeListener { _, value ->
             customMediaNamePreference?.isVisible = (value == "CUSTOM")
             true
